09e60ef7f2958edd6ee324fe22e4ad0a8621ed9a
Ohinken <owenhinken@gmail.com>
2026-03-19 21:01:53 -0600

    Add Mailgun SMTP email config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -150,14 +150,18 @@ GOOGLE_SERVICE_ACCOUNT_FILE = os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE")
 # -------------------------
 # EMAIL
 # -------------------------
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = f"High Desert Auto Detail <{EMAIL_HOST_USER}>"
-BUSINESS_NOTIFICATION_EMAIL = EMAIL_HOST_USER
-
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend"
+)
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.mailgun.org")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+BUSINESS_NOTIFICATION_EMAIL = os.getenv(
+    "BUSINESS_NOTIFICATION_EMAIL",
+    EMAIL_HOST_USER,
+)
 EMAIL_TIMEOUT = 10
